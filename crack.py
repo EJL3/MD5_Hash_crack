79278e6c19b4350556f3559c945b298e059ce3d2
Rizwan Abdul Rahim <rizwrahim@gmail.com>
2021-04-30 06:11:11 +0400

    crk

---
+++ crack.py
@@ -0,0 +1,17 @@
+import hashlib
+flag = 0
+
+pass_hash = input('Enter hashed password =>')
+wordlist = input('Enter password wordlist =>')
+
+for word in wordlist:
+    enc_wrd = word.encode('utf-8')
+    digest = hashlib.md5(enc_wrd.strip()).hexdigest() # can change hash algorithm (hashlib.select type)
+
+    if digest == pass_hash:
+        print('Password found!!')
+        print('Password is:-' + word)
+        flag = 1
+        break
+if flag == 0:
+    print('Password is not in the list' )
